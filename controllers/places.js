766f137523182511c59523a7a2418e72afea6158
Naheed Awan <nadsawan@yahoo.com>
2022-03-04 16:11:59 -0500

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -40,6 +40,17 @@ router.get('/', (req, res) => {
 router.get('/new', (req,res)=>{
     res.render('places/new')
 })
+// show
+
+router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else {
+      res.render('places/show')
+    }
+  })
 
 
 module.exports = router
